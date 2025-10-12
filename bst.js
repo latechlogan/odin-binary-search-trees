f4b471398ce38221e4ead91b60ac49b486e52828
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-10-12 15:56:25 -0500

    Add array of numbers for testing to bst.js

--- bst.js
+++ bst.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
 class Node {
   constructor(data = null, left = null, right = null) {
     this.data = data;
